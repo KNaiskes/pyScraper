6302e91414bafb9aeaa77210bf45c0c08f408df9
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2016-11-09 19:18:07 +0000

    started to organise code in functions, added the option to search tweets based on a word

--- Twitter_scraper.py
+++ Twitter_scraper.py
@@ -1,18 +1,25 @@
 import bs4 as bs
 import urllib.request
 
+def getLatestTweets(profile):
+	tweetsNum = 1
+	print(50 * "*")
+	print("Profile name :",soup.title.text)
+	print("Here are the 21 latest tweets")
+	for tweets in profile.findAll("div", {"class" : "content"}):
+		print("Tweet number :",tweetsNum)
+		print(tweets.find("p").text)
+		tweetsNum = tweetsNum + 1
+	print(50 * "*")
+
+def getTweetsbyword(profile,word):
+	for tweets in profile.findAll("div", {"class" : "content"}):
+		if(word in tweets.text):
+			print(tweets.find("p").text)
+
 source = urllib.request.urlopen("https://twitter.com/lichessorg").read()
 
 soup = bs.BeautifulSoup(source,"html.parser")
-
-print("Here are the 21 latest tweets from")
-print(soup.title.text)
-print(20 * "*")
-tweetsNum = 1
-for tweets in soup.findAll("div", {"class" : "content"}):
-	print("Tweet number :",tweetsNum)
-	print(tweets.find("p").text)
-	tweetsNum = tweetsNum + 1
-
-print(20 * "*")
+#getLatestTweets(soup)
+#getTweetsbyword(soup,"analysis")
 
